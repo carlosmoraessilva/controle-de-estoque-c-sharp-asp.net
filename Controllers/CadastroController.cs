f82988ff41b0582ff984797ac26ab2fe99dd669b
Carlos Moraes <carlosm1412@gmail.com>
2022-03-04 10:30:49 -0300

    associando roles a usuários

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private const int _quantMaxLinhasPorPagina = 5;
+
+        [Authorize]
+        public ActionResult MarcaProduto()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult LocalProduto()
+        {
+            return View();
+        }
+
+       
+
+        [Authorize]
+        public ActionResult Produto()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Pais()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Estado()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Cidade()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Fornecedor()
+        {
+            return View();
+        }
+
+       
+    }
+}
